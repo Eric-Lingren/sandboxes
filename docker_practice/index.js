96f59349ce005b7bf75205732e5e38731eaacd21
Eric Lingren <ericlingren@gmail.com>
2019-09-13 09:11:52 -0600

    adding a docker sandbox

---
+++ docker_practice/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log('Server is Running')
+})
+
+
+// docker build
+// docker run
+// docker ps
+// docker images
