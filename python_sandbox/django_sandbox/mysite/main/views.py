d2262d00299517894db453a716b0c4c5b66d8ea5
Eric Lingren <ericlingren@Erics-MBP.lan>
2022-10-26 21:08:31 -0600

    adding setup script

---
+++ python_sandbox/django_sandbox/mysite/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(response):
+    return HttpResponse('<h1> Hello World! </h1>')
+
+def v1(response):
+    return HttpResponse('<h1> View 1 </h1>') 
